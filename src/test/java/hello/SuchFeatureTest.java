5f2aca0163a2083002a2f88d77212fc96e65ed55
Michael Rauleder <m.rauleder@main-gruppe.de>
2018-01-30 11:53:31 +0100

    Implement test for feature1

---
+++ src/test/java/hello/SuchFeatureTest.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.junit.Test;
+
+public class SuchFeatureTest {
+
+	@Test
+	public void testMuchWow() {
+		
+		SuchFeature.muchWow();
+	}
+
+	@Test
+	public void testVeryFascinate() {
+		
+		SuchFeature.veryFascinate();
+	}
+
+}
